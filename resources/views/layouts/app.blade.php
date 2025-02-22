f1a7cd2d94b7ae04a870999ce3e3cf01bacc462e
JuanCM <juancampuzano2356@gmail.com>
2025-02-22 09:09:08 -0500

    Se actualizan archivos tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title', 'Mi Blog')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
-<body>
+<body class="bg-gray-100 text-gray-900">
     <div class="container mt-4">
         @yield('content')
     </div>
